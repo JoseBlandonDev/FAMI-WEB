ef94f449c116f048376c37cf81cb5c943de32891
Jose David <jdblandondev@gmail.com>
2025-11-23 00:01:17 -0500

    fix: enable standalone output for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
@@ -13,4 +14,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
